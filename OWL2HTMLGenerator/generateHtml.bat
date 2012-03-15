95b424191118f8bdb4ee4bab35341f11cccae4e6
Daniel Garijo <dgarijov@gmail.com>
2012-03-15 02:44:54 +0100

    Bat for generating the 3 ontologies

---
+++ OWL2HTMLGenerator/generateHtml.bat
@@ -0,0 +1,3 @@
+cmd /C java -jar HTMLFromOwlGenerator.jar -uri https://raw.github.com/wf4ever/ro/master/ro.owl -c GeneratedFiles/classRO -p GeneratedFiles/propRO -d GeneratedFiles/dpropRO
+java -jar HTMLFromOwlGenerator.jar -uri https://raw.github.com/wf4ever/ro/master/wfprov.owl -c GeneratedFiles/classWfProv -p GeneratedFiles/propWfProv -d GeneratedFiles/dpropWfprov
+java -jar HTMLFromOwlGenerator.jar -uri https://raw.github.com/wf4ever/ro/master/wfdesc.owl -c GeneratedFiles/classWfDesc -p GeneratedFiles/propWfDesc -d GeneratedFiles/dpropWfDesc
